d3181dd034b1b8a631e33cf26dc6a8078563b075
Steven Buglione <stevenbuglione@Stevens-Mini.fios-router.home>
2022-12-26 19:07:30 -0500

    Inital Navbar Added

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,14 +2,15 @@ import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import 'tailwindcss/tailwind.css';
 import './App.css';
 import SideBar from './components/SideBar/SideBar';
+import NavBar from './components/NavBar/NavBar';
 
 export default function App() {
   return (
     <div className="flex h-screen">
       <SideBar />
-      <main className="flex-1 bg-gray-300">
-        {/* Add your main content here */}
-      </main>
+      <header className="flex-1 bg-gray-300">
+        <NavBar />
+      </header>
     </div>
   );
 }
